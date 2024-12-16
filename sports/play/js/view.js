f86e10cd5017afa0bee02a6f7f36e0bbad868c7a
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2024-12-16 15:15:00 +0530

    Files upload

--- sports/play/js/view.js
+++ sports/play/js/view.js
@@ -43,12 +43,4 @@
 
 
 
-  // Load external HTML content into the body
-        fetch('view.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('content-placeholder').innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Error loading views:', error);
-            });
+  
